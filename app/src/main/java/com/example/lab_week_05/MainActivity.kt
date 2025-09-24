131393f2c5a87075bac6b63ef481ce86bd873ef3
Jeremy Dominic <jeremydag17@gmail.com>
2025-09-24 19:18:31 +0700

    Commit No. 2 - Parse JSON with Moshi and Display Image URL

--- app/src/main/java/com/example/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -8,18 +8,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import com.example.lab_week_05.api.CatApiService
+import com.example.lab_week_05.model.ImageData
+import retrofit2.*
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
-    private val retrofit: Retrofit by lazy {
+    private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://api.thecatapi.com/v1/")
-            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
 
@@ -47,17 +46,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getCatImageResponse() {
-        val call: Call<String> = catApiService.searchImages(1, "full")
-        call.enqueue(object : Callback<String> {
-            override fun onFailure(call: Call<String>, t: Throwable) {
+        val call = catApiService.searchImages(1, "full")
+        call.enqueue(object : Callback<List<ImageData>> {
+            override fun onFailure(call: Call<List<ImageData>>, t: Throwable) {
                 Log.e(MAIN_ACTIVITY, "Failed to get response", t)
             }
 
-            override fun onResponse(call: Call<String>, response: Response<String>) {
+            override fun onResponse(
+                call: Call<List<ImageData>>,
+                response: Response<List<ImageData>>
+            ) {
                 if (response.isSuccessful) {
-                    apiResponseView.text = response.body()
+                    val image = response.body()
+                    val firstImage = image?.firstOrNull()?.imageUrl ?: "No URL"
+                    apiResponseView.text =
+                        getString(R.string.image_placeholder, firstImage)
                 } else {
-                    Log.e(MAIN_ACTIVITY, "Failed to get response\n" + response.errorBody()?.string().orEmpty())
+                    Log.e(
+                        MAIN_ACTIVITY,
+                        "Failed to get response\n" + response.errorBody()?.string().orEmpty()
+                    )
                 }
             }
         })
--- app/src/main/java/com/example/lab_week_05/api/CatApiService.kt
+++ app/src/main/java/com/example/lab_week_05/api/CatApiService.kt
@@ -1,10 +1,14 @@
-package com.example.lab_week_05
+package com.example.lab_week_05.api
 
+import com.example.lab_week_05.model.ImageData
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface CatApiService {
     @GET("images/search")
-    fun searchImages(@Query("limit") limit: Int, @Query("size") size: String): Call<String>
+    fun searchImages(
+        @Query("limit") limit: Int,
+        @Query("size") size: String
+    ): Call<List<ImageData>>
 }
